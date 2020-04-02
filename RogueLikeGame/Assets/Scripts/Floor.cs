f19625505c01b09dd8f3cdff5868905ce0a5f99a
avemaro <yamasakikota@gmail.com>
2020-04-02 23:24:19 +0900

    Test1_1_REFACTORING1

--- RogueLikeGame/Assets/Scripts/Floor.cs
+++ RogueLikeGame/Assets/Scripts/Floor.cs
@@ -4,17 +4,16 @@ using System.IO;
 using UnityEngine;
 
 public class Floor {
+    (int x, int y) floorSize;
     Dictionary<(int x, int y), TerrainType> terrains = new Dictionary<(int x, int y), TerrainType>();
-    Player player;
+    public Player player = new Player();
 
-    public Floor(Player player, string[] floorData) {
-        this.player = player;
+    public Floor(string[] floorData) {
+        floorSize.x = floorData[0].Length;
+        floorSize.y = floorData.Length;
 
-        var sizeX = floorData[0].Length;
-        var sizeY = floorData.Length;
-
-        for (var x = 0; x < sizeX; x++) {
-            for (var y = 0; y < sizeY; y++) {
+        for (var x = 0; x < floorSize.x; x++) {
+            for (var y = 0; y < floorSize.y; y++) {
                 TerrainType terrain = TerrainType.land;
                 switch (floorData[y].ToCharArray()[x]) {
                     case '◆': terrain = TerrainType.wall; break;
@@ -29,5 +28,23 @@ public class Floor {
 
     public TerrainType GetTerrain(int x, int y) {
         return terrains[(x, y)];
+    }
+
+    public void PrintFloor() {
+        for (var x = 0; x < floorSize.x; x++) {
+            var str = "";
+            for (var y = 0; y < floorSize.y; y++) {
+                char data = '　';
+                switch (terrains[(x, y)]) {
+                    case TerrainType.wall: data = '◆'; break;
+                    case TerrainType.water: data = '◇'; break;
+                    case TerrainType.stair: data = '階'; break;
+                }
+                if (player.Position == (x, y)) data = '試';
+
+                str += data;
+            }
+            Debug.Log(str);
+        }
     }
 }
--- RogueLikeGame/Assets/Tests/EditerTests/FeiTests.cs
+++ RogueLikeGame/Assets/Tests/EditerTests/FeiTests.cs
@@ -36,13 +36,13 @@ namespace Tests
 
         [Test]
         public void DangeonDataHasLoaded() {
-            var player = new Player();
-            var floor = new Floor(player, fei1);
+            var floor = new Floor(fei1);
             Assert.AreEqual(TerrainType.wall, floor.GetTerrain(0, 0));
             Assert.AreEqual(TerrainType.land, floor.GetTerrain(1, 1));
             Assert.AreEqual(TerrainType.water, floor.GetTerrain(4, 1));
             Assert.AreEqual(TerrainType.stair, floor.GetTerrain(8, 8));
-            Assert.AreEqual((2, 2), player.Position);
+            Assert.AreEqual((2, 2), floor.player.Position);
+            floor.PrintFloor();
         }
 
 
