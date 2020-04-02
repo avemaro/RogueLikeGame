ce44c382d8d9f9f983fc78f19898c2fd0388d0cc
avemaro <yamasakikota@gmail.com>
2020-04-02 22:00:56 +0900

    Test1_TEST

---
+++ RogueLikeGame/Assets/Tests/EditerTests/FeiTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class FeiTests
+    {
+        //[Test]
+        //public void Test_Fei1() {
+        //    var floor = new Floor("Fei1");
+
+        //}
+
+        [Test]
+        public void DangeonDataHasLoaded() {
+            var floor = new Floor("Fei1");
+            Assert.AreEqual(TerrainType.wall, floor.GetTerrain(0, 0));
+            Assert.AreEqual(TerrainType.land, floor.GetTerrain(1, 1));
+            Assert.AreEqual(TerrainType.water, floor.GetTerrain(4, 1));
+        }
+    }
+}
