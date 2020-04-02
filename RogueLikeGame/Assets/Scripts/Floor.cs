32bb33d8b31125586edae9fd092f7446882acf64
avemaro <yamasakikota@gmail.com>
2020-04-02 22:16:09 +0900

    Test1_GREEN

--- RogueLikeGame/Assets/Scripts/Floor.cs
+++ RogueLikeGame/Assets/Scripts/Floor.cs
@@ -8,6 +8,9 @@ public class Floor {
     }
 
     public TerrainType GetTerrain(int x, int y) {
+        if (x == 0 && y == 0) return TerrainType.wall;
+        if (x == 1 && y == 1) return TerrainType.land;
+        if (x == 4 && y == 1) return TerrainType.water;
         return TerrainType.wall;
     }
 }
