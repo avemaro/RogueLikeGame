5c4731e1cc663be122cfef2559e055ce228fb762
avemaro <yamasakikota@gmail.com>
2020-04-02 22:11:53 +0900

    Test1_RED

---
+++ RogueLikeGame/Assets/Scripts/Floor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor {
+    public Floor(string floorName) {
+
+    }
+
+    public TerrainType GetTerrain(int x, int y) {
+        return TerrainType.wall;
+    }
+}
