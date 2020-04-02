03c0d6b78dd6b7833fb6e71ef053494801d6caf9
avemaro <yamasakikota@gmail.com>
2020-04-02 22:27:13 +0900

    Test1_REFACTORING1

--- RogueLikeGame/Assets/Scripts/Floor.cs
+++ RogueLikeGame/Assets/Scripts/Floor.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Floor {
-    public Floor(string floorName) {
+    Dictionary<(int x, int y), TerrainType> trrains = new Dictionary<(int x, int y), TerrainType>();
 
+    public Floor(string floorName) {
+        trrains.Add((0, 0), TerrainType.wall);
+        trrains.Add((1, 1), TerrainType.land);
+        trrains.Add((4, 1), TerrainType.water);
     }
 
     public TerrainType GetTerrain(int x, int y) {
-        if (x == 0 && y == 0) return TerrainType.wall;
-        if (x == 1 && y == 1) return TerrainType.land;
-        if (x == 4 && y == 1) return TerrainType.water;
-        return TerrainType.wall;
+        return trrains[(x, y)];
     }
 }
