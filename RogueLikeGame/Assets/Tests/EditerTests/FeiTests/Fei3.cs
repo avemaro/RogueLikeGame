09207c024179b7ce10c341afaf27b32eb619f7c2
avemaro <yamasakikota@gmail.com>
2020-04-04 09:51:16 +0900

    Test3_2_TEST

--- RogueLikeGame/Assets/Tests/EditerTests/FeiTests/Fei3.cs
+++ RogueLikeGame/Assets/Tests/EditerTests/FeiTests/Fei3.cs
@@ -39,5 +39,30 @@ namespace Tests
             Assert.AreEqual(item, player.Items[0]);
             Assert.IsNull(floor.GetItem(3, 5));
         }
-   }
+
+        [Test]
+        public void PlayerUseItem() {
+            var floor = new Floor(data);
+            var player = floor.Player;
+            var item = floor.GetItem(3, 5);
+            var enemy = floor.GetEnemy(3, 3);
+            Assert.NotNull(enemy);
+
+            player.Move(Direction.up);
+            player.Use(0);
+            Assert.AreEqual(0, player.Items.Count);
+            Assert.AreEqual(State.Alive, enemy.State);
+
+            floor = new Floor(data);
+            player = floor.Player;
+            enemy = floor.GetEnemy(3, 3);
+
+            player.Move(Direction.up);
+            player.Move(Direction.right);
+            player.Move(Direction.up);
+            player.Use(0);
+            Assert.AreEqual(0, player.Items.Count);
+            Assert.AreEqual(State.Dead, enemy.State);
+        }
+    }
 }
