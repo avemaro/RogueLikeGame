ec5e0e6db3c289303c511e5a558fa16c769d2c31
avemaro <yamasakikota@gmail.com>
2020-04-04 23:12:00 +0900

    Test7_Print_RED

---
+++ RogueLikeGame/Assets/Tests/EditerTests/FeiTests/Fei7.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Fei7
+    {
+        string[] data;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp() {
+            data = new string[] {
+                "◆◆◆◆◆◆◆◆◆",
+                "◆Ｇ     Ｇ◆",
+                "◆　武ＧマＧギ　◆",
+                "◆Ｇ武　マ　ギＧ◆",
+                "◆　武　マ　ギ　◆",
+                "◆　Ｇ　　　Ｇ　◆",
+                "◆　◇◇◇◇◇階◆",
+                "◆Ｇ　試巻　　　◆",
+                "◆◆◆◆◆◆◆◆◆"
+            };
+        }
+
+        [Test]
+        public void Test_floorHasPrinted() {
+            var floor = new Floor(data);
+            Assert.AreEqual(data, floor.PrintFloor());
+        }
+    }
+}
