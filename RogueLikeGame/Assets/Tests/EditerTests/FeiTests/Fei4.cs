2976642ee1009d5b6379c256757c5c1b5234a242
avemaro <yamasakikota@gmail.com>
2020-04-04 14:12:33 +0900

    Fei4_TEST

---
+++ RogueLikeGame/Assets/Tests/EditerTests/FeiTests/Fei4.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests {
+    public class Fei4 {
+        string[] data;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp() {
+            data = new string[] {
+                "◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆",
+                "◆　　　◇　　　◇　　　◇　　　◆",
+                "◆　　　◇　　　◇　　　◇　　　◆",
+                "◆　試杖◇　マ　◇　マ　◇　マ階◆",
+                "◆　　　◇　　　◇　　　◇　　　◆",
+                "◆　　　◇　　　◇　　　◇　　　◆",
+                "◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆"
+            };
+        }
+
+        [Test]
+        public void Test_Fei4() {
+            var floor = new Floor(data);
+            var player = floor.Player;
+            floor.PrintFloor();
+
+            player.Move(Direction.right);
+            player.Use(0);
+            Assert.AreEqual((7, 3), player.Position);
+            player.Use(0);
+            Assert.AreEqual((11, 3), player.Position);
+            player.Throw(0);
+            Assert.AreEqual((15, 3), player.Position);
+            player.Move(Direction.right);
+            Assert.AreEqual(floor.StairPosition, player.Position);
+        }
+    }
+}
