375ba35fbab339df9c34c11f276309fbe22d73dc
avemaro <yamasakikota@gmail.com>
2020-04-05 15:25:43 +0900

    Test10_Printed

---
+++ RogueLikeGame/Assets/Tests/EditerTests/FeiTests/Fei10.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Fei10
+    {
+        string[] data;
+        Floor floor;
+        Player player;
+
+        [SetUp]
+        public void SetUp() {
+            data = new string[] {
+                "◆◆◆◆◆◆◆◆◆◆◆◆◆",
+                "◆　　　　　　　　　　　◆",
+                "◆　　◇　　　　　　　　◆",
+                "◆　試　　　ク　　　　階◆",
+                "◆　　◆　　　　　　　　◆",
+                "◆　　　　　　　　　　　◆",
+                "◆◆◆◆◆◆◆◆◆◆◆◆◆"
+            };
+            floor = new Floor(data);
+            player = floor.Player;
+        }
+
+        [Test]
+        public void Test_floorHasPrinted() {
+            Assert.AreEqual(data, floor.PrintFloor());
+        }
+    }
+}
+
+
