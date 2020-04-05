cb1a6dee990ec8e96ef0b9fa4a75709b1c4206d7
avemaro <yamasakikota@gmail.com>
2020-04-05 19:19:19 +0900

    Test13_TEST

--- RogueLikeGame/Assets/Tests/EditerTests/FeiTests/Fei13.cs
+++ RogueLikeGame/Assets/Tests/EditerTests/FeiTests/Fei13.cs
@@ -35,5 +35,31 @@ namespace Tests
         public void Test_floorHasPrinted() {
             Assert.AreEqual(data, floor.PrintFloor());
         }
+
+        [Test]
+        public void Test_wallHasDigged() {
+            player.Move(Direction.right);
+            player.Equip(0);
+            player.Attack();
+            player.Move(Direction.right);
+            player.Attack();
+            player.Move(Direction.right);
+            player.Attack();
+
+            var expected = new string[] {
+                "◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆",
+                "◆◆◆◆◆◆◆◆Ｇ◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆",
+                "◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆Ｇ◆",
+                "◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆",
+                "◆つ透◆◆◆◆◆◆◆◆◆◆◆◆◆◆☆☆☆◆◆◆◆◆◆◆◆◆◆",
+                "◆試つ◆◆◆Ｇ　　◇　　　◆◆◆◆☆Ｇ☆◆◆◆◆　　　　◆◆",
+                "◆◆◆◆◆◆　　　◇　つ　◆◆◆◆☆☆☆◆◆◆◆　　　武◆◆",
+                "◆◆◆◆◆◆　　　◇　　　◆◆◆◆◆◆◆◆◆◆◆階　　　◆◆",
+                "◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆",
+                "◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆",
+                "◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆"
+            };
+            Assert.AreEqual(expected, floor.PrintFloor());
+        }
     }
 }
