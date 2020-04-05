8a87895a72c5c61e36f472ae16936ab226864023
avemaro <yamasakikota@gmail.com>
2020-04-05 18:57:20 +0900

    deleteGitIgnore

---
+++ RogueLikeGame/Assets/Tests/EditerTests/FeiTests/Fei13.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Fei13
+    {
+        string[] data;
+        Floor floor;
+        Player player;
+
+        [SetUp]
+        public void SetUp() {
+            data = new string[] {
+                "　　　　　　　　　　　◆◆◆◆◆◆◆",
+                "◆◆◆◆◆◆◆◆　　　◆　武　　　◆",
+                "◆　武　　　　◆　　　◆　　　武　◆",
+                "◆　　　　武　◆◆◆◆◆　　　　　◆",
+                "◆試真　武　　　　　　　　真　階武◆",
+                "◆　　　　武　◆◆◆◆◆　　　　　◆",
+                "◆　武　　　　◆　　　◆　　　武　◆",
+                "◆◆◆◆◆◆◆◆　　　◆　武　　　◆",
+                "　　　　　　　　　　　◆◆◆◆◆◆◆"
+            };
+            floor = new Floor(data);
+            player = floor.Player;
+        }
+
+        [Test]
+        public void Test_floorHasPrinted() {
+            Assert.AreEqual(data, floor.PrintFloor());
+        }
+    }
+}
