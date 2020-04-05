45dd23dc3f125d2c7d2828343a503fd61c259d53
avemaro <yamasakikota@gmail.com>
2020-04-05 17:28:31 +0900

    Test12_RED

--- RogueLikeGame/Assets/Scripts/Trap.cs
+++ RogueLikeGame/Assets/Scripts/Trap.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Trap: Stuff {
-    static readonly List<char> IDs = new List<char>() { '罠', '跳' };
+    static readonly List<char> IDs = new List<char>() { '罠', '跳', '爆' };
     public new static Trap Create(Floor floor, Cell cell, char data) {
         if (!IDs.Contains(data)) return null;
         return new Trap(floor, cell, data);
