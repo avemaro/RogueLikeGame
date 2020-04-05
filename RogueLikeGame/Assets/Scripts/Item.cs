89965b500a79555654791502cb89a823b707419a
avemaro <52885446+avemaro@users.noreply.github.com>
2020-04-05 19:09:48 +0900

    Test13_Print

--- RogueLikeGame/Assets/Scripts/Item.cs
+++ RogueLikeGame/Assets/Scripts/Item.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Item : Stuff, IEquatable<Item> {
     static readonly List<char> IDs = new List<char>() { '草', '杖', '巻', '吹',
-        '眼', 'Ｇ', '眠', '縛', '真', '薬' };
+        '眼', 'Ｇ', '眠', '縛', '真', '薬', 'つ', '透' };
     public new static Item Create(Floor floor, Cell cell, char data) {
         if (!IDs.Contains(data)) return null;
         return new Item(floor, cell, data);
