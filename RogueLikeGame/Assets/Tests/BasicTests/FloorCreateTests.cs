4b51376d28d9cbb4a8667e8acfa4a0b2d9d9e5d0
avemaro <yamasakikota@gmail.com>
2020-04-07 19:57:50 +0900

    TwoRoomHasConnected_TEST

---
+++ RogueLikeGame/Assets/Tests/BasicTests/FloorCreateTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class FloorCreateTests
+    {
+        [Test]
+        public void TwoRoomHasConnected() {
+            string[] floorData = {
+                "◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆",
+                "◆◆試　　　　　　◆◆◆◆◆◆　　　　　　　◆◆",
+                "◆◆　　　　　　　◆◆◆◆◆◆　　　　　　　◆◆",
+                "◆◆　　　　　　　◆◆◆◆◆◆　　　　　　　◆◆",
+                "◆◆　　　　　　　◆◆◆◆◆◆　　　　　　　◆◆",
+                "◆◆　　　　　　　　　　　　　　　　　　　　◆◆",
+                "◆◆　　　　　　　◆◆◆◆◆◆　　　　　　　◆◆",
+                "◆◆　　　　　　　◆◆◆◆◆◆　　　　　　　◆◆",
+                "◆◆　　　　　　　◆◆◆◆◆◆　　　　　　　◆◆",
+                "◆◆　　　　　　　◆◆◆◆◆◆　　　　　　階◆◆",
+                "◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆"
+            };
+
+            var floor = new Floor(floorData);
+            floor.PrintFloor();
+
+            Assert.AreEqual(2, floor.Rooms.Count);
+            var player = floor.Player;
+            Assert.AreEqual(floor.Rooms[0], player.Room);
+            player.Move(3, 3, 3, 3);
+
+            for (var i = 0; i < 8; i++) {
+                player.Move(Direction.right);
+                Assert.AreNotEqual(floor.Rooms[1], player.Room);
+            }
+            Assert.AreEqual(floor.Rooms[1], player.Room);
+            floor.PrintFloor();
+        }
+    }
+}
